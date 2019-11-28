ae378ff8a26196c4fc4fea968ee01097481c57fe
Tanner Mindrum <tannermindrum@gmail.com>
2019-11-27 16:57:23 -0800

    added employee table

---
+++ DDL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employee  (
+    empNumber INT(10) NOT NULL,
+    payRate FLOAT(10),
+    empJobTitle VARCHAR(50),
+    empJobDescription VARCHAR(200),
+    fullOrPartTime VARCHAR(50),
+    employeeFunction VARCHAR(100),
+
+    PRIMARY KEY(empNumber)
+);
