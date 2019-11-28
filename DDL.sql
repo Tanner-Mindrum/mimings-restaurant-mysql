bd1b51545b02c66ccccba398f9e8d65f0f5bc9f2
Tanner Mindrum <tannermindrum@gmail.com>
2019-11-27 18:06:18 -0800

    added EmployeeShift junction table

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,6 @@
+/* Creates a parent table, Employee, which contains information regarding
+    an employee at Miming's. It's uniquely identified by empNumber (surrogate)
+    and has a one-to-many relationship with EmployeeShift */
 CREATE TABLE Employee  (
     empNumber INT(10) NOT NULL,
     payRate FLOAT(10),
@@ -6,9 +9,34 @@ CREATE TABLE Employee  (
     fullOrPartTime VARCHAR(50),
     employeeFunction VARCHAR(100),
 
-    PRIMARY KEY(empNumber)
+    CONSTRAINT Employee_PK PRIMARY KEY(empNumber)  -- PK empNumber
 );
 
+/* Creates a parent table, Shift, which contains the time of shift at Miming's.
+    It's uniquely identified by shiftTime and has a one-to-many relationship
+    with EmployeeShift */
 CREATE TABLE Shift (
-    shiftTime TIME PRIMARY KEY
+    shiftTime TIME NOT NULL,
+
+    CONSTRAINT Shift_PK PRIMARY KEY(shiftTime)  -- PK shiftTime
 );
+
+
+/* Creates a junction table, EmployeeShift, which associates Employee and Shift
+    and contains the date of the shift. It's uniquely identified by
+    empNumber (surrogate) and shiftTime and has a one-to-many relationship with
+    Employee and Shift */
+CREATE TABLE EmployeeShift (
+    empNumber INT(10) NOT NULL,
+    shiftTime TIME NOT NULL,
+    empShiftDate DATE,
+    
+    CONSTRAINT EmployeeShift_PK PRIMARY KEY(empNumber, shiftTime),
+
+    CONSTRAINT Employee_EmployeeShift_FK
+        FOREIGN KEY(empNumber) REFERENCES Employee(empNumber),
+
+    CONSTRAINT Shift_EmployeeShift_FK
+        FOREIGN KEY(shiftTime) REFERENCES Shift(shiftTime)
+);
+
