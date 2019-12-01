9d9e316f04de5d1718454d9234a0d3a59a8fb18c
Tanner Mindrum <tannermindrum@gmail.com>
2019-12-01 15:40:27 -0800

    created dml, added employee inserts

---
+++ DML.sql
@@ -0,0 +1,26 @@
+INSERT INTO Employee
+    VALUES (1, 'Head Chef', 'Control and direct food preparation, provide
+                expertise in Chinese cuisine, and create recipes', 'Full time',
+                'Henry Hippo'),
+           (2, 'Line Cook', 'Prepare food to Head Chef''s exact specifications
+                assist Head and Sous Chefs with their daily tasks', 'Full time',
+                'Linus Kook'),
+           (3, 'Sous Chef', 'Plan and direct food preparation in the kitchen and
+                supervise over kitchen staff', 'Full time', 'Sue Lee'),
+           (4, 'Line Cook', 'Prepare food to Head Chef''s exact specifications
+                assist Head and Sous Chefs with their daily tasks', 'Full time',
+                'Won der'),
+           (5, 'Sous Chef', 'Plan and direct food preparation in the kitchen and
+                supervise over kitchen staff', 'Full time', 'Richie Rich'),
+           (6, 'Sous Chef', 'Plan and direct food preparation in the kitchen and
+                supervise over kitchen staff', 'Full time', 'Sandy Rivi'),
+           (7, 'Wait Staff', 'Wait tables and oversee customer service and
+                satisfaction', 'Part time', 'Darlene Cue'),
+           (8, 'Wait Staff', 'Wait tables and oversee customer service and
+                satisfaction', 'Part time', 'Braedon Swift'),
+           (9, 'Dishwasher', 'Wash dishes and glasswear, assist kitchen in prep
+                duties', 'Part time', 'Brenda Medi'),
+           (10, 'Dishwasher', 'Wash dishes and glasswear, assist kitchen in prep
+                duties', 'Part time', 'Javier Monway'),
+           (11, 'Maitre D', 'Manage restaurant seating, lead guests to tables',
+                'Part time', 'Cristoph Freeze');
