966bb4fa089101ef88d7d59c01c27049776c4f36
Tanner Mindrum <tannermindrum@gmail.com>
2019-12-04 09:38:07 -0800

    added query h

--- Queries.sql
+++ Queries.sql
@@ -2,6 +2,10 @@
 SELECT customerName, rewardsPoints FROM Customer NATURAL JOIN MimingsAccount
 ORDER BY rewardsPoints DESC;
 
+-- Query H
+SELECT customerName, amountSpent FROM MimingsAccount NATURAL JOIN Customer
+ORDER BY amountSpent DESC;
+
 -- Query N
 SELECT customerName, organizationName FROM CustomerAccount
 INNER JOIN CorporationAccount on CustomerAccount.customerID = CorporationAccount.customerID
