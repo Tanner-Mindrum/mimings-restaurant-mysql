31a2d3f803f6b7252194c58b2c46b02a7d65c8a2
JoeFreedman24 <38093261+JosephFreedman@users.noreply.github.com>
2019-12-04 09:36:12 -0800

    Query N and O added

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,11 @@
 -- Query G
 SELECT customerName, rewardsPoints FROM Customer NATURAL JOIN MimingsAccount
 ORDER BY rewardsPoints DESC;
+
+-- Query N
+SELECT customerName, organizationName FROM CustomerAccount
+INNER JOIN CorporationAccount on CustomerAccount.customerID = CorporationAccount.customerID
+INNER JOIN Customer on CorporationAccount.customerID = Customer.customerID;
+
+-- Query O
+SELECT * FROM MenuMenuItem;
