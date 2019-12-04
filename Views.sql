803e3d49550ffa0d359193a6edba34640da88c87
stevenone11 <stevencenteno13@gmail.com>
2019-12-04 09:27:45 -0800

    Customer Views

--- Views.sql
+++ Views.sql
@@ -1,1 +1,11 @@
-
+SELECT customerID, customerName, 'Customer' AS "Account Type", email, age, snailMail,'None' AS "corporationName", 'None' AS "organizationName", 'None' AS "officeAddress", 'None' AS contactNumber
+FROM Customer Natural Join CustomerAccount
+WHERE CustomerID IN 
+    (SELECT CustomerID 
+    FROM CustomerAccount)
+UNION
+SELECT customerID, customerName, 'Corporation' AS "Account Type", email, age, 'None' AS "snailMail", corporationName, organizationName, officeAddress, contactNumber 
+FROM Customer Natural Join CorporationAccount
+WHERE CustomerID IN 
+    (SELECT CustomerID 
+    FROM CorporationAccount);
