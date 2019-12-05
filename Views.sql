5a411da1102089ba5c8dce8237dd9676a2ec7925
stevenone11 <stevencenteno13@gmail.com>
2019-12-04 16:23:26 -0800

    View B Finished

--- Views.sql
+++ Views.sql
@@ -1,11 +1,36 @@
+-- Customer Addresses views Prototype (View b)
+SELECT customerID, customerName, 'Corporation' AS "Account Type", email, age, 'None' AS "snailMail", corporationName, organizationName, officeAddress, contactNumber 
+FROM Customer Natural Join CorporationAccount
+WHERE CustomerID IN 
+    (SELECT CustomerID 
+    FROM CorporationAccount)
+UNION
 SELECT customerID, customerName, 'Customer' AS "Account Type", email, age, snailMail,'None' AS "corporationName", 'None' AS "organizationName", 'None' AS "officeAddress", 'None' AS contactNumber
 FROM Customer Natural Join CustomerAccount
 WHERE CustomerID IN 
     (SELECT CustomerID 
     FROM CustomerAccount)
+ORDER BY CustomerID;
+
+-- Customer Addresses views FINISHED (View b)
+SELECT customerID, customerName, 'Both' AS "Account Type", email, age, snailMail, corporationName, organizationName, officeAddress, contactNumber 
+FROM Customer Natural Join CustomerAccount Natural Join CorporationAccount Natural Join MimingsAccount
 UNION
 SELECT customerID, customerName, 'Corporation' AS "Account Type", email, age, 'None' AS "snailMail", corporationName, organizationName, officeAddress, contactNumber 
 FROM Customer Natural Join CorporationAccount
 WHERE CustomerID IN 
     (SELECT CustomerID 
-    FROM CorporationAccount);
+    FROM CorporationAccount) 
+    AND CustomerID NOT IN 
+    (SELECT CustomerID
+    FROM CustomerAccount)
+UNION
+SELECT customerID, customerName, 'Customer' AS "Account Type", email, age, snailMail,'None' AS "corporationName", 'None' AS "organizationName", 'None' AS "officeAddress", 'None' AS contactNumber
+FROM Customer Natural Join CustomerAccount
+WHERE CustomerID IN 
+    (SELECT CustomerID 
+    FROM CustomerAccount)
+    AND CustomerID NOT IN 
+    (SELECT CustomerID 
+    FROM CorporationAccount) 
+ORDER BY CustomerID;
